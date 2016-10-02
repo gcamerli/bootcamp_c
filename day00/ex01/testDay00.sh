c38bfe7e6d7838451ce45ea580d3a4e34339944c
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-02 13:03:18 +0200

    Update testDay00.sh

--- day00/ex01/testDay00.sh
+++ day00/ex01/testDay00.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# sh testDay00.sh
 
 truncate -s 40 testDay00
 chmod 455 testDay00
 touch -t 06012342 testDay00
 tar -cf testDay00.tar testDay00
 rm -f testDay00
+
+# You don't have to push this !
