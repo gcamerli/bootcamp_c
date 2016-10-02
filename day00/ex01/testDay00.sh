e6de1784b0c61864888bd81da78c62f9346f05ec
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-02 14:34:16 +0200

    Add comments

--- day00/ex01/testDay00.sh
+++ day00/ex01/testDay00.sh
@@ -2,9 +2,13 @@
 # sh testDay00.sh
 
 truncate -s 40 testDay00
+# File creation and size mod.
 chmod 455 testDay00
+# Change permissions
 touch -t 06012342 testDay00
+# Change timestamp
 tar -cf testDay00.tar testDay00
 rm -f testDay00
+# Compress archive and deletion
 
 # You don't have to push this !
