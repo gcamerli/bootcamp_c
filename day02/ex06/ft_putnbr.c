a0ef6e2508a9c0c9fb67b13b7cc2c06501dcdebc
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-05 23:30:00 +0200

    Create and add ex06

---
+++ day02/ex06/ft_putnbr.c
@@ -0,0 +1,26 @@
+void	ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	int min;
+	int max;
+
+	min = -2147483648;
+	max = 2147483647;
+	if (nb > min || nb <= max)
+	{
+		if (nb >= 10)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else if (nb < 0)
+		{
+			nb = -nb;
+			ft_putchar('-');
+			ft_putnbr(nb);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+}
