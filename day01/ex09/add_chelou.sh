f805f82f81723b331f29b04670f05b37c8b901e7
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-07 22:25:05 +0200

    Update ex09

--- day01/ex09/add_chelou.sh
+++ day01/ex09/add_chelou.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # sh add_chelou.sh
 
-echo $FT_NBR1 + $FT_NBR2 |\ 
-# Print SUM
-tr "mrdoc" "01234" | tr "'\"?\!\\" "02341" |\
-# Create 2 bases for FT_NBR1 and FT_NBR2
-xargs echo "obase=13; ibase=5;" | bc |\
-# Execute the sum from standard input 
-tr "0123456789ABC" "gtaio luSnemf"
-# Create base for SUM
+#FT_NBR1=\\\'\?\"\\\"\'\\
+#FT_NBR2=rcrdmddd
+
+#FT_NBR1=\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"
+#FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
+
+if ([ "$FT_NBR1" ] && [ "$FT_NBR2" ]); then 
+	echo $FT_NBR2$FT_NBR1 |	tr "'\"?\\\!" "02314" | tr "mrdoc" "01234" |\
+	# Create 2 bases for FT_NBR1 and FT_NBR2
+	xargs echo "obase=13; ibase=5;" | bc |\
+	# Execute the sum from standard input 
+	tr "0123456789ABC" "gtaio luSnemf"
+	# Create base for SUM
+fi
