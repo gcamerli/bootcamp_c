55228fc6e2967a95d7526b208f7a657e75f7d743
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-19 19:57:52 +0200

    Create and add ex04

---
+++ day04/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+int	ft_fibonacci(int index)
+{
+	int result;
+	
+	if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1 || index == 2)
+	{
+		return (1);
+	}
+	else if (index >= 3)
+	{
+		result = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+		return (result);	
+	}
+	else
+	{
+		return (-1);
+	}
+}
