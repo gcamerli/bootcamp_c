f5abfa74119a9da813e2e152d1f371522a2e6b70
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-19 17:02:14 +0200

    Update ex00

--- day04/ex00/ft_iterative_factorial.c
+++ day04/ex00/ft_iterative_factorial.c
@@ -5,14 +5,16 @@ int	ft_iterative_factorial(int nb)
 	
 	i = 1;
 	result = 1;
-	if (nb < 0)
+	if (nb < 0 || nb >= 13)
 	{ 
 		return (0);
 	}
-	while (i <= nb)
+	while (i <= nb && nb <= 12)
 	{
 		result *= i;
 		i++;
 	}
 	return (result);
 }
+
+/* Pay attention to the return int size ! */
