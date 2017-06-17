5a6f08108db1966292b699365a0fca6f04642581
Gius. Camerlingo <gcamerli@protonmail.com>
2017-06-17 19:09:04 +0200

    Create and add rush00

---
+++ solutions/rush00/ex00/rush00.c
@@ -0,0 +1,46 @@
+void	ft_putchar(char c);
+
+char	ft_get_line(int x, char a, char b)
+{
+	int i;
+	
+	if (x == 1)
+	{
+		ft_putchar(a);
+	}
+	if (x > 1)
+	{
+		i = 2; 
+		ft_putchar(a);
+		while (i < x)
+		{
+			ft_putchar(b);
+			i++;
+		}
+		ft_putchar(a);
+	}
+	ft_putchar('\n');
+	return (0);
+}
+
+char	rush(int x, int y)
+{
+	int i;
+	
+	if (y == 1)
+	{
+		ft_get_line(x, 'o', '-');
+	}
+	if (y > 1)
+	{
+		i = 2;
+		ft_get_line(x, 'o', '-');
+		while (i < y)
+		{
+			ft_get_line(x, '|', ' ');
+			i++;
+		}
+		ft_get_line(x, 'o', '-');
+	}
+	return (0);
+}
---
+++ solutions/rush00/ex00/rush01.c
@@ -0,0 +1,46 @@
+void	ft_putchar(char c);
+
+char	ft_get_line(int x, char a, char b, char c)
+{
+	int i;
+	
+	if (x == 1)
+	{
+		ft_putchar(a);
+	}
+	if (x > 1)
+	{
+		i = 2; 
+		ft_putchar(a);
+		while (i < x)
+		{
+			ft_putchar(b);
+			i++;
+		}
+		ft_putchar(c);
+	}
+	ft_putchar('\n');
+	return (0);
+}
+
+char	rush(int x, int y)
+{
+	int i;
+	
+	if (y == 1)
+	{
+		ft_get_line(x, '/', '*', '\\');
+	}
+	if (y > 1)
+	{
+		i = 2;
+		ft_get_line(x, '/', '*', '\\');
+		while (i < y)
+		{
+			ft_get_line(x, '*', ' ', '*');
+			i++;
+		}
+		ft_get_line(x, '\\', '*', '/');
+	}
+	return (0);
+}
