0f49521911c3eb765f19eb78c40979899ae3eea8
Gius. Camerlingo <gcamerli@protonmail.com>
2017-06-23 21:02:09 +0200

    Update ex10

--- solutions/d05/ex10/ft_strcapitalize.c
+++ solutions/d05/ex10/ft_strcapitalize.c
@@ -1,14 +1,37 @@
+int	ft_str_is_alphanum(char c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	if (c >= 97  && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
+
 char *ft_strcapitalize(char *str)
 {
 	int	i;
+	char	*s;
 
 	i = 0;
-	if (str[0] >= 97 && str[0] <= 122)
+	s = str;
+	while (s[i] != '\0')
 	{
-		str[0] = str[0] - 32;
-	}
-	while (str[i] != '\0')
-	{
+		if (ft_str_is_alphanum(s[i - 1]) && s[i] >= 'A' && s[i] <= 'Z')
+		{
+			s[i] += 32;
+		}
+ 		if (!ft_str_is_alphanum(s[i - 1]) && s[i] >= 'a' && s[i] <= 'z')
+		{
+ 			s[i] -= 32;
+		}
 		i++;
 	}
 	return (str);
--- solutions/d05/ex10/main.c
+++ solutions/d05/ex10/main.c
@@ -4,7 +4,8 @@ char	*ft_strcapitalize(char *str);
 
 int	main(void)
 {
-	char test[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char 	test[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	
 	printf("%s \n", test);
 	printf("%s \n", ft_strcapitalize(test));
 	return (0);
