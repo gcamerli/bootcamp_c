cf765663635436324295c8dc76d9eaf8b85d35ca
Gius. Camerlingo <gcamerli@protonmail.com>
2017-08-23 12:03:55 +0200

    Update ex08

--- solutions/d03/ex08/ft_atoi.c
+++ solutions/d03/ex08/ft_atoi.c
@@ -7,12 +7,19 @@ int	ft_atoi(char *str)
 	i = 0;
 	sign = 1;
 	result = 0;
-	if (str[0] == '-')
-	{
-		sign = -1;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
 		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i + 1] >= '0' && str[i + 1] <= '9')
+		{
+			sign = (str[i] == '-') ? -1 : 1;
+			i++;
+		}
+		else
+			return (0);
 	}
-	while (str[i] != '\0')
+	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + str[i] - '0';
 		i++;
